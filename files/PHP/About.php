6fde9f6adbc221802f1b4d92fe04853c6d0976bf
Timo Günther <timo.guenther@stud-mail.uni-wuerzburg.de>
2021-06-07 16:12:15 +0200

    About-site & login and registration on the website

--- files/PHP/About.php
+++ files/PHP/About.php
@@ -1,18 +1,20 @@
-<html>
-<head>
+<!-- Code der Menüleiste -->
 
-<title>F1</title>
-<link rel="stylesheet" href="../CSS/menubar.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
-    <div class="menu-bar">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>About</title>
+    <link rel="stylesheet" href="../CSS/menubar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+  </head>
+  <body>
+  <div class="menu-bar">
     <div class="logo">
       <a href="Home.php"><img src="../../images/F1_Logo.png" alt="" width="140" height="80"/></a>
     </div>
 <ul>
-<li class="active"><a href="#"><i class="fa fa-home"></i>Home</a></li>
+<li><a href="Home.php"><i class="fa fa-home"></i>Home</a></li>
 <li><a href="#"><i class="fa fa-calculator"></i>Statistik</a>
     <div class="sub-menu-1">
         <ul>
@@ -51,9 +53,22 @@
 </li>
 <li><a href="#"><i class="fa fa-trophy"></i>Pokale</a></li>
 <li><a href="#"><i class="fa fa-calendar"></i>Kalender</a></li>
-<li><a href="About.php"><i class="fa fa-info"></i>About</a></li>
+<li class="active"><a href="About.php"><i class="fa fa-info"></i>About</a></li>
 <li><a href="login.php"><i class="fa fa-user-circle-o"></i>Login</a></li>
 </ul>
 </div>
+    <br/>
+<!-- Inhalt der Seite -->
+    <div class="info">
+<h2>Impressum</h2>
+</br>
+<p>Diese Website wurde von Timo Günther für das Projekt in den Digital Humanities an der Julius-Maximilians Universität Würzburg erstellt.</p>
+    </br>
+<p>Gegenstand dieser Website soll es sein einen statistischen Überblick sowohl über die aktuelle als auch die vergangenen Formel 1 Saisons zu geben.</p>
+    </br>
+<p>Die dargestellten Informationen auf dieser Website stammen aus folgenden Quellen:</p>
+    </br>
+<li>https://www.formula1.com/</li>
+
 </body>
 </html>
